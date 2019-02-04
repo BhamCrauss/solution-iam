ab32b1f9e4b5315252e32c892e5aad5820644568
BhamCrauss <46688935+BhamCrauss@users.noreply.github.com>
2019-02-04 16:43:37 -0600

    Add files via upload

---
+++ solution/index.js
@@ -0,0 +1,12 @@
+
+    function showAlert1() {
+      alert ("Hello world!");
+    }
+
+    function showAlert2() {
+    alert ("Hola world!");
+        }
+
+    function showAlert3() {
+     alert ("Hay world!");
+            }
